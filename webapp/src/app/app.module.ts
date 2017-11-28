86b9f037addc94b8d724da70869d962e9c02d299
Tiago Rocha <tmrocha89@gmail.com>
2017-11-28 10:56:57 +0000

    bootstrap added. new component.

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ListsComponent } from './lists/lists/lists.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/list', pathMatch: 'full'},
-  {path: 'list', component: AppComponent /*,data:{title: 'test'}*/},
+  {path: 'list', component: ListsComponent /*,data:{title: 'test'}*/}
   /*{ path: '**', component: PageNotFoundComponent }*/
 ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListsComponent
   ],
   imports: [
     BrowserModule,
