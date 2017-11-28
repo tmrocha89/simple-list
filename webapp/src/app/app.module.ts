90bdcea0915a99c934b288af985381924e500bc5
Tiago Rocha <tmrocha89@gmail.com>
2017-11-28 10:21:40 +0000

    routes

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
-
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/list', pathMatch: 'full'},
+  {path: 'list', component: AppComponent /*,data:{title: 'test'}*/},
+  /*{ path: '**', component: PageNotFoundComponent }*/
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true /*debugging purpouse only*/})
   ],
   providers: [],
   bootstrap: [AppComponent]
